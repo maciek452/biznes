e35e3b3579e54188508ded209a9702f917414984
Jakub <dzejkobjj@gmail.com>
2018-01-13 16:22:47 +0100

    eciepecie2

--- src/main/java/pl/apoeldevelopers/bizneselektroniczny/controllers/MainController.java
+++ src/main/java/pl/apoeldevelopers/bizneselektroniczny/controllers/MainController.java
@@ -1,17 +1,29 @@
 package pl.apoeldevelopers.bizneselektroniczny.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.apoeldevelopers.bizneselektroniczny.MyObj;
+import pl.apoeldevelopers.bizneselektroniczny.entity.UserRating;
 
 @RestController
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public MyObj getMyObj(){
         MyObj myObj = new MyObj();
         myObj.setMessage("Ostateczna próba");
 
         return myObj;
+    }
+
+    @PostMapping("/test")
+    ResponseEntity<?> test(@RequestBody UserRating input){
+        if(input.getLorem() == "ipsum")
+            return ResponseEntity.ok().build();
+        else
+            return ResponseEntity.notFound().build();
     }
 }

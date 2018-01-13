5f747f6b1797859feeb79cd589780bded7a4fd90
Jakub <dzejkobjj@gmail.com>
2018-01-13 17:43:03 +0100

    eciepecie2

--- src/main/java/pl/apoeldevelopers/bizneselektroniczny/Config.java
+++ src/main/java/pl/apoeldevelopers/bizneselektroniczny/Config.java
@@ -20,8 +20,8 @@ public class Config {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        //dataSource.setUrl("jdbc:mysql://172.17.0.2:3306/mahout");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mahout");
+        dataSource.setUrl("jdbc:mysql://172.17.0.2:3306/mahout");
+        //dataSource.setUrl("jdbc:mysql://localhost:3306/mahout");
         dataSource.setUsername("user1");
         dataSource.setPassword("Buterier123");
 

cdb27f86269a4856f1511199e59b3adf720f33e7
Jakub <dzejkobjj@gmail.com>
2018-01-15 00:19:02 +0100

    eciepecie2

--- src/main/java/pl/apoeldevelopers/bizneselektroniczny/Config.java
+++ src/main/java/pl/apoeldevelopers/bizneselektroniczny/Config.java
@@ -25,8 +25,8 @@ public class Config {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        //dataSource.setUrl("jdbc:mysql://172.17.0.2:3306/mahout?verifyServerCertificate=false&useSSL=true");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mahout?verifyServerCertificate=false&useSSL=true");
+        dataSource.setUrl("jdbc:mysql://172.17.0.2:3306/prestashop?verifyServerCertificate=false&useSSL=true");
+        //dataSource.setUrl("jdbc:mysql://localhost:3306/mahout?verifyServerCertificate=false&useSSL=true");
         dataSource.setUsername("user1");
         dataSource.setPassword("Buterier123");
 
